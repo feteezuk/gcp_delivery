c1ecd84e3746cc90cac46cccfe03770d04a3397d
Jonathan <feteezuk@gmail.com>
2022-08-15 20:58:08 +0000

    add bob to main.py

--- main.py
+++ main.py
@@ -14,6 +14,12 @@ def name(value):
     val = {'value': value}
     return jsonify(val)
 
+@app.route('/bob')
+def bob():
+    val = {'value': 'bob'}
+    return jsonify(val)
+    
+
 
 if __name__ == '__main__':
 
